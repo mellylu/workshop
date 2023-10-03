32705f5eeb499620016272f9628dd6603b997898
Melly Lucas <melly.lucas32@yahoo.fr>
2023-10-03 09:50:06 +0200

    commitheaderfooter

--- components/header/header.js
+++ components/header/header.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react"
+import { AiOutlineLogout } from "react-icons/ai"
+import styles from "./header.module.scss"
 
 const Header = () => {
     return (
-        <div>
-            
-        </div>
-    );
-};
+        <header className={styles.header}>
+            <nav className={styles.menu}>
+                <ul className={styles.ul}>
+                    <AiOutlineLogout
+                        size={30}
+                        color="#61dafb"
+                        onClick={() => {
+                            logout()
+                        }}
+                        className={styles.logout}
+                    />
+                </ul>
+            </nav>
+        </header>
+    )
+}
 
-export default Header;
+export default Header
--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,13 @@ import Header from "../components/header/header"
 import "../styles/styles.scss"
 
 function MyApp({ Component, pageProps }) {
-  return <div><Component {...pageProps}></Component><Footer/></div>
+    return (
+        <div>
+            <Header />
+            <Component {...pageProps}></Component>
+            <Footer />
+        </div>
+    )
 }
 
 export default MyApp
